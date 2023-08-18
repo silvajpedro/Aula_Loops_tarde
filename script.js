cd3d6c3516b60bd7fa7563848916ef1424320b61
silvajpedro <silvajpro.31@gmail.com>
2023-08-18 16:27:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+let contador = 0
+
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+
+
+
+
+
+// OPERADORES DE ATRIBUIÇÃO
+
+
+// atribuição  de adição
+// +=
+let segundoContador = 0
+
+// forma completa
+//                      0         + 1
+segundoContador = segundoContador + 1
+console.log(segundoContador)
+
+// forma simplificada
+segundoContador += 1
+
+console.log(segundoContador)
+
+segundoContador += 3
+
+console.log(segundoContador)
+
+let precoTotal = 0
+
+console.log(precoTotal += 15)
+
+console.log(precoTotal += 18)
+
+console.log(precoTotal += 28)
+
+// atribução de subtração
+// -=
+
+console.log(precoTotal -= 12)
+
+console.log(precoTotal -= 8)
+
+let segundoPrecoTotal = 8.50
+
+console.log(segundoPrecoTotal)
+
+console.log(segundoPrecoTotal -= 4.5)
+
+// atribuição de multiplicação
+// *=
+
+console.log(segundoPrecoTotal *= 3)
+
+// atribuição de divisão
+// /=
+
+console.log(segundoPrecoTotal /= 2)
+
+
+let terceiroContador = 0
+
+// LOOPS
+// Estruturas de código, que irão executar uma ação repetidas vezes, até que a condição seja verdadeira.
+
+
+// while
+// enquanto
+
+// while( aqui vai a nossa condição ){
+//     aqui vai a nossa ação, que será repetida diversas vezes
+// }
+
+let quartoContador = 0
+
+
+
+while(quartoContador < 100){
+    console.log(quartoContador += 1)
+}
+
+// let quintoContador = 100
+
+// while(quintoContador >-100){
+//     console.log(quintoContador -= 1)
+// }
+
+// for
+// para a nossa variável contador = 0
+// enquanto ela for menor ou igual a 25
+// faça contador += 1
+for(let contador = 0; contador <= 25; contador+=1 ){
+    console.log(contador)
+}
+
+// do{} while()
